16a715fd43451c32a0c7996cb784a25f7b82caea
Emily Tang <emitang@mit.edu>
2015-06-26 18:05:50 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Sun Bear Designs
+
+The website for an online store selling custom hand-painted shoes.
+
+## The Laravel Framework was used for this project
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	/titles and stuff go here
+</head>
+<body>
+	<header>
+		/top bar maybe
+	</header>
+	<content>
+		/here go the sun bears!
+	</content>
+</body>
+</html>
